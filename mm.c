435948c4f6b12fa3f163f55efd0d40526dc99c0b
Anthony Incorvati <ari5086@psu.edu>
2022-06-30 19:43:18 -0400

    Commenting and small variable name changing

--- mm.c
+++ mm.c
@@ -51,77 +51,69 @@
 #define memcpy mem_memcpy
 #endif /* DRIVER */
 
-/* What is the correct alignment? */
 #define ALIGNMENT 16
 
+// Size of chunk, head/foot, and double head/foot
+size_t HEAD_SIZE = 8;
+size_t DHEAD_SIZE = 16;  
 
-//basic size constants (Computer Sytems, A Programmer's Perspective)
-size_t WSIZE = 8;  /* Word and header/footer size (bytes) */
-size_t DSIZE = 16;   /* double Word and header/footer size (bytes) */
-int CHUNKSIZE = (1<<12); /* Extend heap by this amount (bytes) ----- WHY this arithmatic?? */
-static char *heap_list = 0;
+// Initialization of the starting address for the heap
+static char *heap_start = 0;
 
-
-
-uint64_t MAX(int x, int y)
-{
-    return ((x) > (y) ? (x) : (y));
-}
-
-uint64_t PACK(int size,  int alloc) //Pack size and allocated bit to a word
+// Uses bitwise operators to return a package of size and allocation ready to be placed into the heap
+uint64_t PACK(int size,  int alloc)
 {
     return ((size) | (alloc));
 }
- 
-uint64_t PUT(char *p, uint64_t val) //write word at address p
+
+//Writes val at given address
+uint64_t PUT(char *addr, uint64_t val) 
 {
-    return (*(uint64_t *)(p) = (uint64_t)(val));
+    return (*(uint64_t *)(addr) = (uint64_t)(val));
 } 
 
-uint64_t GET(char *p) //reads word at address p
+//Reads at given address and returns package
+uint64_t GET(char *addr)
 {
-    return (*(unsigned int *)(p));
+    return (*(unsigned int *)(addr));
 } 
 
 //  return the size from header or footer at address 
-uint64_t GET_SIZE(char *p) 
+uint64_t GET_SIZE(char *addr) 
 {
-    return (GET(p) & ~(DSIZE - 1));
+    return (GET(addr) & ~(DHEAD_SIZE - 1));
 }
 // return the allocated bit from header or footer at address 
-uint64_t GET_ALLOC(char *p) 
+uint64_t GET_ALLOC(char *addr) 
 {
-    return (GET(p) & 0x1);
+    return (GET(addr) & 0x1);
 }
     
-
-char *HDRP(void *bp) //return header pointer for bp
+// return address to the header
+char *HDRP(void *addr) 
 {
-    return (char*)(bp) - WSIZE;
+    return (char*)(addr) - HEAD_SIZE;
 }
 
-char *FTRP(char *bp) //return footer pointer bp
+// return address to the footer
+char *FTRP(char *addr) 
 {
-    return ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE); 
+    return ((char *)(addr) + GET_SIZE(HDRP(addr)) - DHEAD_SIZE); 
 } 
 
 
-// return the block pointers of the next block
-char *NEXT_BLKP(void *bp) 
+// return the address of the next block
+char *NEXT_ADDR(void *addr) 
 {
-    return ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)));
+    return ((char *)(addr) + GET_SIZE(((char *)(addr) - HEAD_SIZE)));
 }
 
-// return the block pointers of the previous block
+// return the address of the previous block
 
-char *PREV_BLKP(void *bp)
+char *PREV_ADDR(void *addr)
 {
-    return ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)));
+    return ((char *)(addr) - GET_SIZE(((char *)(addr) - DHEAD_SIZE)));
 } 
-
-// return next pointer
-
-
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 static size_t align(size_t x)
@@ -129,87 +121,99 @@ static size_t align(size_t x)
     return ALIGNMENT * ((x+ALIGNMENT-1)/ALIGNMENT);
 }
 
-void *find_fit(size_t fitsize)
+// First fit algorithm to find the first available space in the heap for the given size
+void *find_fit(size_t size)
 {
-    void *bp;
+    void *addr;
 
-    for (bp = heap_list; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) 
+    for (addr = heap_start; GET_SIZE(HDRP(addr)) > 0; addr = NEXT_ADDR(addr)) 
     {
-        if (!GET_ALLOC(HDRP(bp)) && (fitsize <= GET_SIZE(HDRP(bp))))  
+        if (!GET_ALLOC(HDRP(addr)) && (size <= GET_SIZE(HDRP(addr))))  
         {
-            return bp;
+            return addr;
         }
     }
     return NULL;
 }
 
-void place(void *bp, size_t asize)
+// Places a header and footer into the heap at a given address and deals with splitting if necessary
+void place(void *addr, size_t new_size)
 {
-    size_t csize = GET_SIZE(HDRP(bp));
+    size_t old_size = GET_SIZE(HDRP(addr));
 
-    if ((csize - asize) >= (2*DSIZE)) 
+    //If splitting is necessary
+    if ((old_size - new_size) >= (2*DHEAD_SIZE)) 
     {
-        PUT(HDRP(bp), PACK(asize, 1));
-        PUT(FTRP(bp), PACK(asize, 1));
-        bp = NEXT_BLKP(bp);
-        PUT(HDRP(bp), PACK(csize-asize, 0));
-        PUT(FTRP(bp), PACK(csize-asize, 0));
+        PUT(HDRP(addr), PACK(new_size, 1));
+        PUT(FTRP(addr), PACK(new_size, 1));
+        addr = NEXT_ADDR(addr);
+        PUT(HDRP(addr), PACK(old_size-new_size, 0));
+        PUT(FTRP(addr), PACK(old_size-new_size, 0));
     }
+    //If splitting is not necessary
     else
     {
-        PUT(HDRP(bp), PACK(csize, 1));
-        PUT(FTRP(bp), PACK(csize, 1));
+        PUT(HDRP(addr), PACK(old_size, 1));
+        PUT(FTRP(addr), PACK(old_size, 1));
     }
 }
 
-void *coalesce(void *bp)
+//Coalesces free blocks at given address
+void *coalesce(void *addr)
 {
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    size_t size = GET_SIZE(HDRP(bp));
+    size_t prev = GET_ALLOC(FTRP(PREV_ADDR(addr)));
+    size_t next = GET_ALLOC(HDRP(NEXT_ADDR(addr)));
+    size_t size = GET_SIZE(HDRP(addr));
 
-    if (prev_alloc && next_alloc) { /* Case 1 */
-        return bp;
+    // CASE 1: No coalescing needed
+    if (prev && next) { 
+        return addr;
     }
 
-    else if (prev_alloc && !next_alloc) { /* Case 2 */
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-        PUT(HDRP(bp), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size,0));
+    // CASE 2: Coalesce the next block
+    else if (prev && !next) {
+        size += GET_SIZE(HDRP(NEXT_ADDR(addr)));
+        PUT(HDRP(addr), PACK(size, 0));
+        PUT(FTRP(addr), PACK(size,0));
     }
 
-    else if (!prev_alloc && next_alloc) { /* Case 3 */
-        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-        PUT(FTRP(bp), PACK(size, 0));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
+    // CASE 3: Coalesce the previous block
+    else if (!prev && next) { 
+        size += GET_SIZE(HDRP(PREV_ADDR(addr)));
+        PUT(FTRP(addr), PACK(size, 0));
+        PUT(HDRP(PREV_ADDR(addr)), PACK(size, 0));
+        addr = PREV_ADDR(addr);
     }
  
-    else { /* Case 4 */
-        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
-        GET_SIZE(FTRP(NEXT_BLKP(bp)));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
+    // CASE 4: Coalesce both blocks
+    else { 
+        size += GET_SIZE(HDRP(PREV_ADDR(addr))) +
+        GET_SIZE(FTRP(NEXT_ADDR(addr)));
+        PUT(HDRP(PREV_ADDR(addr)), PACK(size, 0));
+        PUT(FTRP(NEXT_ADDR(addr)), PACK(size, 0));
+        addr = PREV_ADDR(addr);
     }
-    return bp;
+    return addr;
 }
-void *extend_heap(size_t words)
-{
-    char *bp;
-    
 
-    /* Allocate an even number of words to maintain alignment */
-    if ((long)(bp = mem_sbrk(words)) == -1)
+// Extends the heap using mem_sbrk function of given size
+void *extend_heap(size_t size)
+{
+    char *addr;
+
+    // Allocate an even number of words to maintain alignment 
+    if ((long)(addr = mem_sbrk(size)) == -1)
     return NULL;
 
-    /* Initialize free block header/footer and the epilogue header */
-    PUT(HDRP(bp), PACK(words, 0)); /* Free block header */
-    PUT(FTRP(bp), PACK(words, 0)); /* Free block footer */
-    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
+    // Initialize free block header/footer and the buffer header 
+    PUT(HDRP(addr), PACK(size, 0));  
+    PUT(FTRP(addr), PACK(size, 0)); 
+    PUT(HDRP(NEXT_ADDR(addr)), PACK(0, 1)); 
 
-    /* Coalesce if the previous block was free */
-    return coalesce(bp);
+    mm_checkheap(__LINE__);
+
+    // Coalesce if the previous block was free 
+    return coalesce(addr);
  }
 
 /*
@@ -218,19 +222,20 @@ void *extend_heap(size_t words)
 bool mm_init(void)
 {
     
-
     /* Create the initial empty heap */
-    if ((heap_list = mem_sbrk(4*WSIZE)) == (void *)-1)
+    if ((heap_start = mem_sbrk(4*HEAD_SIZE)) == (void *)-1)
     {
         return false;
     }
-    PUT(heap_list, 0); /* Alignment padding */
-    PUT(heap_list + (1*WSIZE), PACK(DSIZE, 1)); /* Prologue header */
-    PUT(heap_list + (2*WSIZE), PACK(DSIZE, 1)); /* Prologue footer */
-    PUT(heap_list + (3*WSIZE), PACK(0, 1)); /* Epilogue header */
-    heap_list += (2*WSIZE);
 
-    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+    // Create padding/buffer, header, and footer into heap for intitialization
+    PUT(heap_start, 0); 
+    PUT(heap_start + (1*HEAD_SIZE), PACK(DHEAD_SIZE, 1)); 
+    PUT(heap_start + (2*HEAD_SIZE), PACK(DHEAD_SIZE, 1)); 
+    PUT(heap_start + (3*HEAD_SIZE), PACK(0, 1)); 
+    heap_start += (2*HEAD_SIZE);
+
+    if (extend_heap((1<<12)/HEAD_SIZE) == NULL)
     {
         return false;
     }
@@ -245,28 +250,29 @@ void* malloc(size_t size)
 {
 
     size_t asize; 
-    char *bp;
+    char *addr;
 
     if (size == 0)
     return NULL;
 
-    asize = align(size)+DSIZE;
+    asize = align(size)+DHEAD_SIZE;
 
-    if ((bp = find_fit(asize))!=NULL)
+    // If there is a fit in the heap, place a new header and footer there
+    if ((addr = find_fit(asize)) != NULL)
     {
-        place(bp,asize);
-        return bp;
+        place(addr,asize);
+        return addr;
     }
     
-    
-
-    
-    if ((bp = extend_heap(asize)) == NULL)
+    // Request more room in the heap and check to make sure it's granted
+    if ((addr = extend_heap(asize)) == NULL)
     {
         return NULL;
     }
-    place(bp, asize);
-    return bp;
+
+    // Place header and footer at new address in the heap
+    place(addr, asize);
+    return addr;
     
 
 }
@@ -279,8 +285,11 @@ void free(void* ptr)
 {
     size_t size = GET_SIZE(HDRP(ptr));
 
+    // Put a header and footer at the given address containing a pack of size and allocation boolean
     PUT(HDRP(ptr), PACK(size, 0)); 
     PUT(FTRP(ptr), PACK(size, 0)); 
+
+    // Run new free block address through coalesce function to check if coalecsing is necessary
     coalesce(ptr);
 
     return;
@@ -291,6 +300,8 @@ void free(void* ptr)
  */
 void* realloc(void* oldptr, size_t size)
 {
+
+    // Check if given parameters are valid
     if (size == 0)
     {
         free(oldptr);
@@ -298,17 +309,18 @@ void* realloc(void* oldptr, size_t size)
     }
     else if (oldptr == NULL)
     {
-        char *bp = malloc(size);
-        return bp;
+        char *addr = malloc(size);
+        return addr;
     }
+
     else
     {
         size_t old_size = GET_SIZE(HDRP(oldptr));
-        size_t new_size = align(size) +DSIZE ;
+        size_t new_size = align(size) +DHEAD_SIZE ;
 
+        // Perform malloc and free operation if given size is less than the existing one at given address
         if(old_size < new_size)
         {
-          
             void *new_ptr = malloc(new_size);
             memcpy(new_ptr, oldptr, old_size);
             free(oldptr);
@@ -337,8 +349,6 @@ void* calloc(size_t nmemb, size_t size)
 }
 
 
-
-
 static bool in_heap(const void* p)
 {
     return p <= mem_heap_hi() && p >= mem_heap_lo();
@@ -357,11 +367,25 @@ static bool aligned(const void* p)
 /*
  * mm_checkheap
  */
-bool mm_checkheap(int lineno)
-{
-#ifdef DEBUG
-    /* Write code to check heap invariants here */
-    /* IMPLEMENT THIS */
-#endif /* DEBUG */
+bool mm_checkheap(int lineno) {
+
+    void *addr = heap_start;
+
+    dbg_printf("---------------- MM CHECK HEAP %d ----------------\n", lineno);
+    dbg_printf("     Mem Heap Lo|     Mem Heap Hi|\n");
+    dbg_printf("%16lx|%16lx|\n", (uint64_t)(mem_heap_lo() - mem_heap_lo()), (uint64_t)(mem_heap_hi() - mem_heap_lo()));
+
+    int count = 0;
+    while(addr < mem_heap_hi() - DHEAD_SIZE){
+        dbg_printf("---------------------------------------------------\n");
+        dbg_printf("%6d Head Size|       Head Free|    Head Address|\n", count);
+        dbg_printf("%16lx|%16lx|%16lx|\n", GET_SIZE(HDRP(addr)), GET_ALLOC(HDRP(addr)), GET(HDRP(addr)));        
+        // dbg_printf("       Foot Size|       Foot Free|    Foot Address|\n");
+        // dbg_printf("%16lx|%16lx|%16lx|\n", GET_SIZE(FTRP(addr)), GET_ALLOC(FTRP(addr)), GET(FTRP(addr)));  
+
+        count += 1;
+        addr = NEXT_ADDR(addr);
+    }
     return true;
+
 }
